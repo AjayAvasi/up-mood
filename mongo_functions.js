9d9feacdc70fbdc8f19bc24f72c5e9b459781567
AjayAvasi <79114792+AjayAvasi@users.noreply.github.com>
2023-12-11 17:29:02 -0500

    API updates

---
+++ mongo_functions.js
@@ -0,0 +1,186 @@
+const { MongoClient } = require('mongodb');
+const mongoUrl = 'mongodb://localhost:27017';
+const dbName = 'up-mood';
+const client = new MongoClient(mongoUrl); 
+
+
+async function createUser(user) {
+    try {
+        // Connect to MongoDB
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('users');
+
+        // Check if the user already exists
+        const existingUser = await collection.findOne({ "username": user.username });
+
+        if (existingUser) {
+            return 409; // Conflict - User already exists
+        }
+
+        // Insert the new user
+        await collection.insertOne(user);
+        return 200; // OK - User created successfully
+    } catch (error) {
+        console.error('Error creating user:', error);
+        return 500; // Internal Server Error
+    } finally {
+        await client.close();
+    }
+}
+async function loginUser(user) {
+    try {
+        // Connect to MongoDB
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('users');
+
+        // Check if the user exists
+        const existingUser = await collection.findOne({ "username": user.username, "password": user.password });
+
+        if (!existingUser) {
+            return 401; // Unauthorized - Invalid username or password
+        }
+
+        return 200; // OK - User logged in successfully
+    } catch (error) {
+        console.error('Error login user:', error);
+        return 500; // Internal Server Error
+    } finally {
+        await client.close();
+    }
+}
+async function findUserByID(id) {
+    try {
+        // Connect to MongoDB
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('users');
+
+        // Check if the user exists
+        const existingUser = await collection.findOne({ "_id": id });
+
+        return existingUser; // OK
+    } catch (error) {
+        console.error('Error finding user:', error);
+        return 500; // Internal Server Error
+    } finally {
+        await client.close();
+    }
+}
+async function findUserByUsername(username) {
+    try {
+        // Connect to MongoDB
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('users');
+
+        // Check if the user exists
+        const existingUser = await collection.findOne({ "username": username });
+
+
+        return existingUser; // OK
+    } catch (error) {
+        console.error('Error finding user:', error);
+        return 500; // Internal Server Error
+    } finally {
+        await client.close();
+    }
+}
+
+async function friendRequest(user_id, friend_username) {
+    try {
+        // Connect to MongoDB
+        
+        const friendUser = await findUserByUsername(friend_username);
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('friends');
+        if (!friendUser) {
+            return 404; // Not Found - User not found
+        }
+        const friend_id = friendUser._id;
+        const existingFriendRequest = await collection.findOne({"$or": [{"origin": user_id, "target": friend_id}, {"origin": friend_id, "target": user_id}]});
+        if (existingFriendRequest) {
+            return 409; // Conflict - Friend request already exists
+        }
+        await collection.insertOne({"origin": user_id, "target": friend_id, "status": "pending"});
+        return 200; // OK - Friend request sent successfully
+    } catch (error) {
+        console.error('Error requesting friend from user:', error);
+        return 500; // Internal Server Error
+    } finally {
+        await client.close();
+    }
+}
+
+async function acceptFriendRequest(user_id, friend_username) {
+    try {
+        const friendUser = await findUserByUsername(friend_username);
+        if (!friendUser) {
+            return 404; // Not Found - User not found
+        }
+        const friend_id = friendUser._id;
+        // Connect to MongoDB
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('friends');
+
+        // Check if the user exists
+        const existingFriendRequest = await collection.findOne({"origin": friend_id, "target": user_id, "status": "pending"});
+        if (!existingFriendRequest) {
+            return 404; // Not Found - Friend request not found
+        }
+        await collection.updateOne({"origin": friend_id, "target": user_id}, {$set: {"status": "accepted"}});
+        return 200; // OK - Friend request accepted successfully
+    } catch (error) {
+        console.error('Error accepting friend request:', error);
+        return 500; // Internal Server Error
+    } finally {
+        await client.close();
+    }
+}
+async function rejectFriendRequest(user_id, friend_username) {
+    try {
+        const friendUser = await findUserByUsername(friend_username);
+        if (!friendUser) {
+            return 404; // Not Found - User not found
+        }
+        const friend_id = friendUser._id;
+        // Connect to MongoDB
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('friends');
+
+        // Check if the user exists
+        const existingFriendRequest = await collection.findOne({"origin": friend_id, "target": user_id, "status": "pending"});
+        if (!existingFriendRequest) {
+            return 404; // Not Found - Friend request not found
+        }
+        await collection.deleteOne({"origin": friend_id, "target": user_id});
+        return 200; // OK - Friend request accepted successfully
+    } catch (error) {
+        console.error('Error accepting friend request:', error);
+        return 500; // Internal Server Error
+    } finally {
+        await client.close();
+    }
+}
+async function getFriends(user_id) {
+    try {
+        // Connect to MongoDB
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('friends');
+
+        const existingFriends = await collection.find({"$or": [{"origin": user_id}, {"target": user_id}]}).toArray();
+        return existingFriends; // OK
+    } catch (error) {
+        console.error('Error finding user:', error);
+        return 500; // Internal Server Error
+    } finally {
+        await client.close();
+    }
+}
+
+module.exports = { createUser, loginUser, findUserByID, findUserByUsername, friendRequest, acceptFriendRequest, getFriends};
---
+++ server.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const {connect, close, createUser, loginUser, findUserByID, findUserByUsername, friendRequest, acceptFriendRequest, getFriends} = require('./mongo_functions');
+const { ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 3000;
+
+secretKey = "akjfhasdkjlfhkjsdf";
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  if(!req.cookies.jwt) {
+    res.redirect('/login');
+  }else {
+  res.send('Hello, World!');
+  }
+});
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html');
+}); 
+app.post('/api/signup', (req, res) => {
+    const user = req.body;
+    
+    createUser(user).then(result => {
+        res.status(result).send();
+    }).catch(error => {
+      res.status(500).send();
+      console.error(error);
+    });
+    
+});
+
+app.post('/api/login', (req, res) => {
+    const user = req.body;
+    
+    loginUser(user).then(result => {
+        if (result === 200) {
+          findUserByUsername(user.username).then(user => {
+            const token = jwt.sign( {id: user._id.toString()} , secretKey, { expiresIn: "1h" });
+            res.cookie('jwt', token);
+             res.json({ message: 'Login successful' });
+          }).catch(error => {
+            res.status(500).send();
+            console.error(error);
+          });
+        }else {
+          res.status(result).send();
+        }
+        
+    }).catch(error => {
+      res.status(500).send();
+      console.error(error);
+    });
+    
+});
+app.post('/api/sendFriendRequest', (req, res) => {
+  const friend_username = req.body.username;
+  const token = req.cookies.jwt;
+  const decoded = jwt.verify(token, secretKey);
+  const id = new ObjectId(decoded.id);
+  friendRequest(id, friend_username).then(result => {
+    res.status(result).send();
+  }).catch(error => {
+    res.status(500).send();
+    console.error(error);
+  });
+ 
+});
+
+app.post('/api/acceptFriendRequest', (req, res) => {
+  const friend_username = req.body.username;
+  const token = req.cookies.jwt;
+  const decoded = jwt.verify(token, secretKey);
+  const id = new ObjectId(decoded.id);
+  acceptFriendRequest(id, friend_username).then(result => {
+    res.status(result).send();
+  }).catch(error => {
+    res.status(500).send();
+    console.error(error);
+  });
+ 
+});
+app.get('/api/getFriends', (req, res) => {
+  const token = req.cookies.jwt;
+  const decoded = jwt.verify(token, secretKey);
+  const id = new ObjectId(decoded.id);
+  getFriends(id).then(result => {
+    res.json(result);
+  }).catch(error => {
+    res.status(500).send();
+    console.error(error);
+  });
+ 
+}
+);
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
